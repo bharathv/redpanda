aade8d607f493fb2006f695abb2dde4836075d77
Bharath Vissapragada <bharathv@redpanda.com>
2022-11-30 15:33:49 -0800

    tx/metrics: Add metrics for inflight requests

--- src/v/cluster/rm_stm.h
+++ src/v/cluster/rm_stm.h
@@ -292,6 +292,7 @@ protected:
     ss::future<> handle_eviction() override;
 
 private:
+    void setup_metrics();
     ss::future<> do_remove_persistent_state();
     ss::future<std::vector<rm_stm::tx_range>>
 
@@ -739,9 +740,9 @@ private:
     config::binding<std::chrono::seconds> _log_stats_interval_s;
     ss::timer<clock_type> _log_stats_timer;
     prefix_logger _ctx_log;
-
     config::binding<uint64_t> _max_concurrent_producer_ids;
     mutex _clean_old_pids_mtx;
+    ss::metrics::metric_groups _metrics;
 };
 
 } // namespace cluster
