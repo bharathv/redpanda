01b185df6a5b7c2af66faca16c1e2319e2101d83
Bharath Vissapragada <bharathv@redpanda.com>
2023-08-30 15:09:07 -0700

    features: add new feature idempotency_v2
    This feature flag is needed so that we can write the local snapshots
    in a backward compatible way until the upgrade is fully done. If at any
    point the upgrade got rolled back, we do not want the snapshots to be
    incompatible with older RP versions.

--- src/v/features/feature_table.h
+++ src/v/features/feature_table.h
@@ -66,6 +66,7 @@ enum class feature : std::uint64_t {
     cloud_storage_scrubbing = 1ULL << 32U,
     enhanced_force_reconfiguration = 1ULL << 33U,
     broker_time_based_retention = 1ULL << 34U,
+    idempotency_v2 = 1ULL << 35U,
 
     // Dummy features for testing only
     test_alpha = 1ULL << 61U,
@@ -315,7 +316,12 @@ constexpr static std::array feature_schema{
     feature::broker_time_based_retention,
     feature_spec::available_policy::new_clusters_only,
     feature_spec::prepare_policy::always},
-};
+  feature_spec{
+    cluster::cluster_version{11},
+    "idempotency_v2",
+    feature::idempotency_v2,
+    feature_spec::available_policy::always,
+    feature_spec::prepare_policy::always}};
 
 std::string_view to_string_view(feature);
 std::string_view to_string_view(feature_state::state);
