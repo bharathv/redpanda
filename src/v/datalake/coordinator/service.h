48818f60b51bc64ecfaed9d85de9bc6e46f8ee90
Bharath Vissapragada <bharathv@redpanda.com>
2024-09-24 12:29:46 -0700

    datalake/coordinator/rpc: add rpc service implementation

---
+++ src/v/datalake/coordinator/service.h
@@ -0,0 +1,30 @@
+// Copyright 2024 Redpanda Data, Inc.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.md
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0
+#include "datalake/coordinator/rpc_service.h"
+
+namespace datalake::coordinator {
+class frontend;
+}
+
+namespace datalake::coordinator::rpc {
+class service final : public impl::datalake_coordinator_rpc_service {
+public:
+    service(
+      ss::scheduling_group, ss::smp_service_group, ss::sharded<frontend>*);
+
+    ss::future<add_translated_data_files_reply> add_translated_data_files(
+      add_translated_data_files_request, ::rpc::streaming_context&) override;
+
+    ss::future<fetch_latest_data_file_reply> fetch_latest_data_file(
+      fetch_latest_data_file_request, ::rpc::streaming_context&) override;
+
+private:
+    ss::sharded<frontend>* _frontend;
+};
+} // namespace datalake::coordinator::rpc
