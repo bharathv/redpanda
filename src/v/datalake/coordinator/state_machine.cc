50c3ef80eb8bbe0e2e4640d1c1e16cbe8939b834
Bharath Vissapragada <bharathv@redpanda.com>
2024-09-24 11:53:17 -0700

    datalake/coordinator: barebones coordinator stm implementation
    The logic is for demonstration purposes only, will be refactored later.

---
+++ src/v/datalake/coordinator/state_machine.cc
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2024 Redpanda Data, Inc.
+ *
+ * Use of this software is governed by the Business Source License
+ * included in the file licenses/BSL.md
+ *
+ * As of the Change Date specified in that file, in accordance with
+ * the Business Source License, use of this software will be governed
+ * by the Apache License, Version 2.0
+ */
+
+#include "datalake/coordinator/state_machine.h"
+
+#include "datalake/logger.h"
+
+namespace datalake::coordinator {
+
+coordinator_stm::coordinator_stm(ss::logger& logger, raft::consensus* raft)
+  : raft::persisted_stm<>("datalake_coordinator_stm.snapshot", logger, raft) {}
+
+ss::future<> coordinator_stm::do_apply(const model::record_batch&) {
+    co_return;
+}
+
+model::offset coordinator_stm::max_collectible_offset() { return {}; }
+
+ss::future<>
+coordinator_stm::apply_local_snapshot(raft::stm_snapshot_header, iobuf&&) {
+    co_return;
+}
+
+ss::future<raft::stm_snapshot>
+coordinator_stm::take_local_snapshot(ssx::semaphore_units) {
+    return ss::make_exception_future<raft::stm_snapshot>(
+      std::runtime_error{"not implemented exception"});
+}
+
+ss::future<> coordinator_stm::apply_raft_snapshot(const iobuf&) { co_return; }
+
+ss::future<iobuf> coordinator_stm::take_snapshot(model::offset) {
+    co_return iobuf{};
+}
+
+ss::future<add_translated_data_files_reply>
+coordinator_stm::add_translated_data_file(add_translated_data_files_request) {
+    co_return add_translated_data_files_reply{coordinator_errc::ok};
+}
+
+ss::future<fetch_latest_data_file_reply>
+coordinator_stm::fetch_latest_data_file(fetch_latest_data_file_request) {
+    co_return fetch_latest_data_file_reply{coordinator_errc::ok};
+}
+
+bool stm_factory::is_applicable_for(const storage::ntp_config& config) const {
+    const auto& ntp = config.ntp();
+    return (ntp.ns == model::datalake_coordinator_nt.ns)
+           && (ntp.tp.topic == model::datalake_coordinator_topic);
+}
+
+void stm_factory::create(
+  raft::state_machine_manager_builder& builder, raft::consensus* raft) {
+    auto stm = builder.create_stm<coordinator_stm>(datalake_log, raft);
+    raft->log()->stm_manager()->add_stm(stm);
+}
+
+} // namespace datalake::coordinator
---
+++ src/v/datalake/coordinator/state_machine.h
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2024 Redpanda Data, Inc.
+ *
+ * Use of this software is governed by the Business Source License
+ * included in the file licenses/BSL.md
+ *
+ * As of the Change Date specified in that file, in accordance with
+ * the Business Source License, use of this software will be governed
+ * by the Apache License, Version 2.0
+ */
+#pragma once
+#include "cluster/state_machine_registry.h"
+#include "datalake/coordinator/types.h"
+#include "raft/persisted_stm.h"
+
+namespace datalake::coordinator {
+
+class coordinator_stm final : public raft::persisted_stm<> {
+public:
+    static constexpr std::string_view name = "datalake_coordinator_stm";
+
+    explicit coordinator_stm(ss::logger&, raft::consensus*);
+
+    ss::future<> do_apply(const model::record_batch&) override;
+
+    model::offset max_collectible_offset() override;
+
+    ss::future<>
+    apply_local_snapshot(raft::stm_snapshot_header, iobuf&& bytes) override;
+
+    ss::future<raft::stm_snapshot>
+      take_local_snapshot(ssx::semaphore_units) override;
+
+    ss::future<> apply_raft_snapshot(const iobuf&) final;
+
+    ss::future<iobuf> take_snapshot(model::offset) final;
+
+    ss::future<add_translated_data_files_reply>
+      add_translated_data_file(add_translated_data_files_request);
+
+    ss::future<fetch_latest_data_file_reply>
+      fetch_latest_data_file(fetch_latest_data_file_request);
+
+private:
+};
+class stm_factory : public cluster::state_machine_factory {
+public:
+    stm_factory() = default;
+    bool is_applicable_for(const storage::ntp_config&) const final;
+    void create(raft::state_machine_manager_builder&, raft::consensus*) final;
+};
+} // namespace datalake::coordinator
