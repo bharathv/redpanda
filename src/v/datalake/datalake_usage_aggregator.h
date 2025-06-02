ca1a373cd0d46836f7e8cccfa9c113ecbc2d1a91
Bharath Vissapragada <bharathv@redpanda.com>
2025-06-02 13:07:24 -0700

    dl/usage: add an implementation for datalake_usage_api

--- src/v/datalake/datalake_usage_aggregator.h
+++ src/v/datalake/datalake_usage_aggregator.h
@@ -11,11 +11,15 @@
 
 #pragma once
 
+#include "base/seastarx.h"
+#include "cluster/fwd.h"
+#include "datalake/fwd.h"
 #include "kafka/server/datalake_usage_api.h"
 
 namespace cluster {
 class controller;
 }
+#include <seastar/core/sharded.hh>
 
 namespace datalake {
 
@@ -28,5 +32,19 @@ public:
 
 private:
     cluster::controller* _controller{nullptr};
+};
+
+class default_datalake_usage_api_impl final : public kafka::datalake_usage_api {
+public:
+    explicit default_datalake_usage_api_impl(
+      cluster::controller*,
+      ss::sharded<cluster::topic_table>*,
+      ss::sharded<datalake::coordinator::frontend>*);
+    ss::future<usage_stats> compute_usage(ss::abort_source&) final;
+
+private:
+    cluster::controller* _controller;
+    ss::sharded<cluster::topic_table>* _topics;
+    ss::sharded<coordinator::frontend>* _frontend;
 };
 } // namespace datalake
