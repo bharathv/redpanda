9a2a35b2fb6c43e55aa4c83d854c62a2fc6de08e
Michael Boquard <michael@redpanda.com>
2025-09-26 16:25:41 -0400

    k/client: Provide accessor to cluster
    Signed-off-by: Michael Boquard <michael@redpanda.com>

--- src/v/cluster_link/replication/mux_remote_consumer.h
+++ src/v/cluster_link/replication/mux_remote_consumer.h
@@ -74,6 +74,8 @@ public:
     ss::future<std::expected<partition_data_queue::fetch_data, errc>>
     fetch(const ::model::topic_partition&, ss::abort_source&);
 
+    kafka::client::cluster& cluster() { return _consumer->get_cluster(); }
+
 private:
     bool should_throttle_produce();
     bool can_ignore_partition_data(const ::model::topic_partition&);
